b2b9f270b48d115919c359b11923e766a23f0aea
kitekate <cwallez@hotmail.fr>
2018-04-10 07:58:06 +0200

    survol component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+/* decorateur*/
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  /* propriété avec = valeur assignée*/
   title = 'app';
+  name = 'Micheline';
+
+  /* propriété avec : type assignée */
+  aNumber: number;
+
+
+  /* méthode avec fonctions entre {} */
+  onUSerInput(event) {
+    this.name = event.target.value;
+  }
 }
