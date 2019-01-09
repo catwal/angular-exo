406e95a19daf8974ee9b78e2ea8066b9244914b5
catwal <kitekate.cw@gmail.com>
2019-01-09 14:47:10 +0100

    binding et mis en place des roots app

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
-
+  public character;
   constructor() { }
 
   ngOnInit() {
--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.component.css']
 })
 export class TabsComponent implements OnInit {
+public characters = [
+  {name: 'Luke Sky', side: ''},
+  {name: 'Dark Vador', side: ''}];
+public  chosenList = 'all';
 
   constructor() { }
 
   ngOnInit() {
   }
+  onChoose() {
 
+  }
+
+  getCharacters() {
+    if (this.chosenList === 'all') {
+      return this.chosenList.slice();
+    }
+    return this.characters.filter((char) => {
+      return char.side === this.chosenList;
+    });
+  }
 }
